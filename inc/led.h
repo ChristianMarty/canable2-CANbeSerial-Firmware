cb0e4b9de166f709e852d9f351113c7de750d554
Christian Marty <info@christian-marty.ch>
2024-02-11 17:37:08 +0100

    Change green LED  pin to match Makerbase CANable V2.0 hardware

--- inc/led.h
+++ inc/led.h
@@ -6,8 +6,8 @@
 #define LED_BLUE_Port GPIOA
 #define LED_BLUE LED_BLUE_Port , LED_BLUE_Pin
 
-#define LED_GREEN_Pin GPIO_PIN_11
-#define LED_GREEN_Port GPIOB
+#define LED_GREEN_Pin GPIO_PIN_0
+#define LED_GREEN_Port GPIOA
 #define LED_GREEN LED_GREEN_Port , LED_GREEN_Pin
 
 
